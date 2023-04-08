9a1c7879aa69ee4ef2214b9e2fcfd69534532c7e
pourya-momtaz <pouryamz19@gmail.com>
2023-04-08 15:57:32 +0430

    first commit

---
+++ src/test/java/calculator/MyStepdefs.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import calculator.Calculator;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+public class MyStepdefs {
+    private Calculator calculator;
+    private int value;
+    private String operation;
+    private int result;
+
+    @Before
+    public void before() {
+        calculator = new Calculator();
+    }
+
+    @Given("^One input value and operation, (-?\\d+) and (sqr|rvs)$")
+    public void oneInputValueAndOperation(int arg0, String arg1) {
+        value = arg0;
+        operation = arg1;
+    }
+
+    @When("^I do the operation on the value$")
+    public void iReverseTheValue() {
+        if (operation == "sqr"){
+            result = calculator.sqrt(value);
+        }
+        else if (operation == "rvs") {
+            result = calculator.reverse(value);
+        }
+        System.out.print(result);
+    }
+
+    @Then("^I expect the result (-?\\d+)$")
+    public void iExpectTheResult(int arg0) {
+        Assert.assertEquals(arg0, result);
+    }
+}
---
+++ src/test/resources/features/calculator.feature
@@ -0,0 +1,6 @@
+@tag
+Feature: Calculator
+  Scenario: reverse a number
+    Given One input value and operation, 68821 and sqr
+    When I do the operation on the value
+    Then I expect the result 12886
